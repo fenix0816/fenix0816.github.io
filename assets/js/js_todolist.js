e38c0f2c3cf1bc5ac5a1b359dd6122d0cd098439
fenix0816 <fenix.huang0816@gmail.com>
2023-12-12 19:18:27 +0800

    1212

--- assets/js/js_todolist.js
+++ assets/js/js_todolist.js
@@ -2,7 +2,7 @@ let elInput = document.querySelector('#todo-in');
 let elAddBtn = document.querySelector('#todo-add-btn');
 let elItem = document.querySelector('#todo-item');
 
-elAddBtn.addEventListener('click', (e) => {
+const addTodo = () => {
     let value = elInput.value;
     if (!value) {
         elInput.focus();
@@ -10,10 +10,42 @@ elAddBtn.addEventListener('click', (e) => {
     }
 
     elItem.innerHTML += `<li>
-                            <div><input type="checkbox"></div>
-                            <div>${value}</div>
+                            <input type="checkbox">
+                            <span>${value}</span>
                         </li>`
 
     elInput.value = '';
     elInput.focus();
+}
+
+elAddBtn.addEventListener('click', (e) => {
+    addTodo();
+})
+
+
+elInput.addEventListener('keyup', (e) => {
+    if (e.key.toString().toUpperCase() == 'ENTER') {
+        addTodo();
+    }
+})
+
+elItem.addEventListener('click', (e) => {
+    let el = e.target;
+    let tag = el.tagName.toString().toUpperCase();
+    let isCheckbox = false;
+
+    if (tag == 'INPUT') {
+        isCheckbox = true;
+    }
+
+    if (tag == 'SPAN' || tag == 'INPUT') {
+        el = el.parentNode
+    }
+
+    if (el.tagName.toString().toUpperCase() == 'LI') {
+        let checkbox = el.querySelector('input[type=checkbox]');
+        if (!isCheckbox) {
+            checkbox.checked = !checkbox.checked;
+        }
+    }
 })
