d58534326d53b50b2bf9d3249bf32420c90e5359
fenix0816 <fenix.huang0816@gmail.com>
2023-12-21 21:22:40 +0800

    1221-3

--- assets/js/js_todolist_realtime.js
+++ assets/js/js_todolist_realtime.js
@@ -1,25 +1,30 @@
 import { TodoRealtime } from './class/TodoRealtime.js';
-import { UID } from './class/UID.js';
+// import { UID } from './class/UID.js';
 import { App } from './Firebase/App.js';
 import { Auth } from './Firebase/Auth.js';
 
 const app = await App.init();
 const auth = new Auth(app);
 
-let email = 'kindping@gmail.com'
-let password = '12345678'
-
-// let user = await auth.register(email, password);
-let user = await auth.signIn(email, password);
-console.log(user);
-
-
-
 let todoApp = document.querySelector('#todo-app');
 let uidApp = document.querySelector('#uid-app');
-let uid = UID.read();
+let isRegisterMode = false;
 
-if (uid) {
+// let user = await auth.register(email, password);
+// let user = await auth.signIn(email, password);
+// console.log(user);
+
+const authed = async (user) => {
+    if (isRegisterMode) {
+        await Swal.fire({
+            title: '註冊成功',
+            html: `已註冊，請登入`,
+            icon: 'success'
+        })
+        location.reload();
+        return;
+    }
+    let uid = user.uid;
     todoApp.classList.add('active');
     // TODO Application.
     let elInput = document.querySelector('#todo-in');
@@ -53,25 +58,124 @@ if (uid) {
         }
     })
 
-    elChangeUid.addEventListener('click', (e) => {
+    elChangeUid.addEventListener('click', async (e) => {
         e.preventDefault();
-        UID.clear();
+        await auth.signOut()
         location.reload();
     })
 
 
     let elCurentUid = document.querySelector('#current-uid');
     elCurentUid.innerHTML = uid
-} else {
-    uidApp.classList.add('active');
+}
 
-    let elUid = document.querySelector('#todo-uid');
-    let elBtn = document.querySelector('#todo-uid-btn');
-    elBtn.addEventListener('click', (e) => {
-        let value = elUid.value;
-        if (value) {
-            UID.write(value);
-            location.reload();
+const unauthed = () => {
+    uidApp.classList.add('active');
+    let elAccount = document.querySelector('#todo-account');
+    let elPassword = document.querySelector('#todo-password');
+
+    let elSignInBtn = document.querySelector('#todo-signin-btn')
+    elSignInBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+        let account = elAccount.value;
+        let password = elPassword.value;
+        if (!account) {
+            await Swal.fire({
+                title: '登入失敗',
+                html: '帳號未填寫',
+                icon: 'error'
+            })
+            setTimeout(() => {
+                elAccount.focus();
+            }, 500)
+            return
+        }
+
+        if (!password) {
+            await Swal.fire({
+                title: '登入失敗',
+                html: '密碼未填寫',
+                icon: 'error'
+            })
+            setTimeout(() => {
+                elPassword.focus();
+            }, 500)
+            return
+        }
+
+        isRegisterMode = false;
+
+        let user = await auth.signIn(account, password);
+        if (user) {
+            uidApp.classList.remove('active');
+            Swal.fire({
+                title: '登入成功',
+                html: `已登入: ${account}`,
+                icon: 'success'
+            })
+        } else {
+            Swal.fire({
+                title: '登入失敗',
+                html: '帳號密碼錯誤',
+                icon: 'error'
+            })
         }
     })
+
+    let elRegisterBtn = document.querySelector('#todo-register-btn')
+
+    elRegisterBtn.addEventListener('click', async (e) => {
+        e.preventDefault();
+
+        let account = elAccount.value;
+        let password = elPassword.value;
+        if (!account) {
+            await Swal.fire({
+                title: '註冊失敗',
+                html: '帳號未填寫',
+                icon: 'error'
+            })
+            setTimeout(() => {
+                elAccount.focus();
+            }, 500)
+            return
+        }
+
+        if (!password) {
+            await Swal.fire({
+                title: '註冊失敗',
+                html: '密碼未填寫',
+                icon: 'error'
+            })
+            setTimeout(() => {
+                elPassword.focus();
+            }, 500)
+            return
+        }
+
+        isRegisterMode = true;
+        let user = await auth.register(account, password);
+        if (user) {
+            await auth.signOut();
+            elAccount.value = ''
+            elPassword.value = ''
+        } else {
+            Swal.fire({
+                title: '註冊失敗',
+                html: '請稍後再試',
+                icon: 'error'
+            })
+        }
+    })
+
+    // let elUid = document.querySelector('#todo-uid');
+    // let elBtn = document.querySelector('#todo-uid-btn');
+    // elBtn.addEventListener('click', (e) => {
+    //     let value = elUid.value;
+    //     if (value) {
+    //         location.reload();
+    //     }
+    // })
 }
+
+auth.onChange(authed, unauthed);
